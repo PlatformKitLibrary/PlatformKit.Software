907267b1b0575bb757db208ec9bf9171d796245c
Alastair Lundy <133523182+alastairlundy@users.noreply.github.com>
2024-04-16 19:54:22 +0100

    update code to reflect update in dependencies

--- Helpers/InstalledFlatpaks.cs
+++ Helpers/InstalledFlatpaks.cs
@@ -1,57 +1,85 @@
-﻿namespace PlatformKit.Software;
+﻿/*
+        MIT License
 
-public class InstalledFlatpaks
+        Copyright (c) 2024 Alastair Lundy
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+
+   */
+
+namespace PlatformKit.Software
 {
-    /// <summary>
-    /// Platforms Supported On: Linux and FreeBsd.
-    /// </summary>
-    /// <returns></returns>
-    /// <exception cref="PlatformNotSupportedException"></exception>
-    public static AppModel[] Get()
+
+// ReSharper disable once ClassNeverInstantiated.Global
+    public class InstalledFlatpaks
     {
-        List<AppModel> apps = new List<AppModel>();
-
-        if (PlatformAnalyzer.IsLinux() || PlatformAnalyzer.IsFreeBSD())
+        /// <summary>
+        /// Platforms Supported On: Linux and FreeBsd.
+        /// </summary>
+        /// <returns></returns>
+        /// <exception cref="PlatformNotSupportedException"></exception>
+        public static AppModel[] Get()
         {
-            bool useFlatpaks;
-            
-            string[] flatpakTest = CommandRunner.RunCommandOnLinux("flatpak --version").Split(' ');
-                
-            if (flatpakTest[0].Contains("Flatpak"))
-            {
-                Version.Parse(flatpakTest[1]);
+            List<AppModel> apps = new List<AppModel>();
 
-                useFlatpaks = true;
-            }
-            else
+            if (OperatingSystem.IsLinux() || OperatingSystem.IsFreeBSD())
             {
-                useFlatpaks = false;
-            }
+                bool useFlatpaks;
 
-            if (useFlatpaks)
-            {
+                string[] flatpakTest = CommandRunner.RunCommandOnLinux("flatpak --version").Split(' ');
+
+                if (flatpakTest[0].Contains("Flatpak"))
+                {
+                    Version.Parse(flatpakTest[1]);
+
+                    useFlatpaks = true;
+                }
+                else
+                {
+                    useFlatpaks = false;
+                }
+
+                if (useFlatpaks)
+                {
 #if NET5_0_OR_GREATER
                 string[] flatpakResults = CommandRunner.RunCommandOnLinux("flatpak list --columns=name")
                     .Split(Environment.NewLine);
 #else
-                string[] flatpakResults = CommandRunner.RunCommandOnLinux("flatpak list --columns=name")
-                    .Split(Convert.ToChar(Environment.NewLine));
+                    string[] flatpakResults = CommandRunner.RunCommandOnLinux("flatpak list --columns=name")
+                        .Split(Convert.ToChar(Environment.NewLine));
 #endif
 
-                var installLocation = CommandRunner.RunCommandOnLinux("flatpak --installations");
-                
-                foreach (var flatpak in flatpakResults)
-                {
-                    apps.Add(new AppModel(flatpak, installLocation));
+                    var installLocation = CommandRunner.RunCommandOnLinux("flatpak --installations");
+
+                    foreach (var flatpak in flatpakResults)
+                    {
+                        apps.Add(new AppModel(flatpak, installLocation));
+                    }
+
+                    return apps.ToArray();
                 }
-                
+
+                apps.Clear();
                 return apps.ToArray();
             }
 
-            apps.Clear();
-            return apps.ToArray();
+            throw new PlatformNotSupportedException();
         }
-
-        throw new PlatformNotSupportedException();
     }
 }
--- Helpers/InstalledSnaps.cs
+++ Helpers/InstalledSnaps.cs
@@ -1,38 +1,66 @@
-﻿namespace PlatformKit.Software;
+﻿/*
+        MIT License
+
+        Copyright (c) 2024 Alastair Lundy
+
+        Permission is hereby granted, free of charge, to any person obtaining a copy
+        of this software and associated documentation files (the "Software"), to deal
+        in the Software without restriction, including without limitation the rights
+        to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+        copies of the Software, and to permit persons to whom the Software is
+        furnished to do so, subject to the following conditions:
+
+        The above copyright notice and this permission notice shall be included in all
+        copies or substantial portions of the Software.
+
+        THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+        IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+        FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+        AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+        LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+        OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+        SOFTWARE.
+
+   */
 
 using PlatformKit;
 
-public class InstalledSnaps
+
+namespace PlatformKit.Software
 {
-    /// <summary>
-    /// Detect what Snap packages (if any) are installed on a linux distribution or on macOS.
-    /// </summary>
-    /// <returns>Returns a list of installed snaps. Returns an empty array if no Snaps are installed.</returns>
-    /// <exception cref="PlatformNotSupportedException">Throws an exception if run on a Platform other than Linux, macOS, and FreeBsd.</exception>
-    public static AppModel[] Get()
+
+    public class InstalledSnaps
     {
-        List<AppModel> apps = new List<AppModel>();
-
-        if (PlatformAnalyzer.IsLinux() || PlatformAnalyzer.IsLinux() || PlatformAnalyzer.IsFreeBSD())
+        /// <summary>
+        /// Detect what Snap packages (if any) are installed on a linux distribution or on macOS.
+        /// </summary>
+        /// <returns>Returns a list of installed snaps. Returns an empty array if no Snaps are installed.</returns>
+        /// <exception cref="PlatformNotSupportedException">Throws an exception if run on a Platform other than Linux, macOS, and FreeBsd.</exception>
+        public static AppModel[] Get()
         {
-            bool useSnap = Directory.Exists("/snap/bin");
+            List<AppModel> apps = new List<AppModel>();
 
-            if (useSnap)
+            if (OperatingSystem.IsLinux() || OperatingSystem.IsFreeBSD())
             {
-                string[] snapResults = CommandRunner.RunCommandOnLinux("ls /snap/bin").Split(' ');
+                bool useSnap = Directory.Exists("/snap/bin");
 
-                foreach (var snap in snapResults)
+                if (useSnap)
                 {
-                    apps.Add(new AppModel(snap, "/snap/bin"));
+                    string[] snapResults = CommandRunner.RunCommandOnLinux("ls /snap/bin").Split(' ');
+
+                    foreach (var snap in snapResults)
+                    {
+                        apps.Add(new AppModel(snap, "/snap/bin"));
+                    }
+
+                    return apps.ToArray();
                 }
 
+                apps.Clear();
                 return apps.ToArray();
             }
 
-            apps.Clear();
-            return apps.ToArray();
+            throw new PlatformNotSupportedException();
         }
-
-        throw new PlatformNotSupportedException();
     }
 }
